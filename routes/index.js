75011a9b77c2a89d63301c51c503f0163ab4af04
cgrenard <cngrenard22@gmail.com>
2025-04-14 09:55:49 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
 
+//import abcRoutes from './abc.js';
+//import xyzRoutes from './xyz.js';
+
+const constructorMethod = (app) => {
+  //app.use('/abc', abcRoutes);
+  //app.use('/xyz', xyzRoutes);
+
+  app.use('*', (req, res) => {
+    res.status(404).json({error: 'Route Not found'});
+  });
+};
+
+export default constructorMethod;
